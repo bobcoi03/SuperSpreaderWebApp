37359dcfd1bc630702a541c248be4317d23ceec5
bobcoi03 <elongatedmusk6969@gmail.com>
2022-05-11 23:16:14 +0100

    drew a circle & canvas

--- src/public/scripts/game.js
+++ src/public/scripts/game.js
@@ -1,1 +1,16 @@
 PIXI.utils.sayHello();
+
+function main() {
+	let app = new PIXI.Application({width: 640, height: 480, backgroundColor: 0x3440eb });
+	let ticker = PIXI.Ticker
+	document.body.appendChild(app.view);
+
+	let graphics = new PIXI.Graphics();
+	graphics.beginFill(0xffffff);
+	graphics.drawCircle(30, 30, 20);
+	graphics.endFill(); 
+
+	app.stage.addChild(graphics);
+}
+
+main();
